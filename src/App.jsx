ead6ab0a5efeffd36f1c8e083833f09eb935d34c
asouzay <161075027+asouzay@users.noreply.github.com>
2024-03-13 22:12:00 -0400

    fhfhfh
    NFJFJF

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+import "./globals.css";
+
+
+
+
 export default function App() {
 
-  const nome = "João";
-  const sobreNome = "Santos";
+  const nome = "Mariany";
+  const sobreNome = "Souza";
 
   return(
     <div>
         <h1>Hello World</h1>
-        <p>Desenvolvido por: {nome}</p>
+        <p>Desenvolvido por: Mariany</p>
     </div>  
   );
 
