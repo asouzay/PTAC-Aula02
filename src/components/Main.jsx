92b28ee05e4ab7ca3db9121a5bf9304609df6f0d
asouzay <maryani.silva@estudante.ifms.edu.br>
2024-03-29 08:58:23 -0400

    Update Main.js

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,6 +3,22 @@ import { useState } from "react";
 export default function Main(){
     const [nome, setNome] = useState("");
     const [Number, setNumber] = useState(0)
+    const [listaContatos,setLista] = useState ([]);
+    const [ CPF, setCpf] = useState (0);
+
+
+    const registrar = (event) =>{
+        event.preventDefault();
+       alert("ola")
+       setLista([...listaContatos,
+     {
+       nomeSalvo: nome,
+       telefoneSalvo: telefone
+     }
+     ]);
+    };
+     console.table(listaContatos);
+
     return(
     <main>
         <form>
@@ -12,6 +28,24 @@ export default function Main(){
       
           <input type="text" name=""  id="" onChange={(event) => setNumber(event.target.value)}/>
           {Number}
+
+          <label htmfor="telefone">telefone:</label>
+         <input 
+       type="number"
+        nome="" id=""
+         onChange={(event)=> setTelefone(event.target.event)}
+         />
+         {telefone}
+
+         <label htmfor="C
+         PF">CPF:</label>
+         <input 
+       type="number"
+        Cpf="" id=""
+         onChange={(event)=> setCpf(event.target.event)}
+         />
+         {CPF}
+         <button>enviar</button>
          </form>
     </main>
     );
